a6fe84aa5bc9267f210b1c193f0e0863f7c526b3
BryannX <bngugi13@alaalumni.org>
2017-05-03 11:11:44 +0300

    Updated the Movie Adapter and added the Picasso dependency.

--- app/src/main/java/com/example/android/movies/Movie.java
+++ app/src/main/java/com/example/android/movies/Movie.java
@@ -1,14 +1,68 @@
 package com.example.android.movies;
 
+import android.content.Context;
+
 /**
  * Created by brianngugi on 30-Apr-17.
  */
 
 public class Movie {
-    int[] images;
+    private String movie_title;
+    private String poster_path;
+    private String plot;
+    private String rating;
+    private String release_date;
 
-    public Movie(int[] images)
+    public Movie(String title, String poster, String mPlot, String mRating, String date)
     {
-        this.images =
+        this.movie_title = title;
+        this.poster_path = poster;
+        this.plot = mPlot;
+        this.rating = mRating;
+        this.release_date = date;
+    }
+
+    //Class Setters
+    public void setTitle(String title)
+    {
+        this.movie_title = title;
+    }
+    public void setPoster_path(String poster)
+    {
+        this.poster_path = poster;
+    }
+    public void setPlot(String mPlot)
+    {
+        this.plot = mPlot;
+    }
+    public void setRating(String mRating)
+    {
+        this.rating = mRating;
+    }
+    public void setRelease_date(String date)
+    {
+        this.release_date = date;
+    }
+
+    //Class Getters
+    public String getMovie_title()
+    {
+        return movie_title;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getRelease_date() {
+        return release_date;
     }
 }
--- app/src/main/java/com/example/android/movies/MovieAdapter.java
+++ app/src/main/java/com/example/android/movies/MovieAdapter.java
@@ -1,15 +1,19 @@
 package com.example.android.movies;
 
 import android.app.Activity;
+import android.content.Context;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -20,51 +24,58 @@ import java.util.List;
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder>{
     private static final String TAG = MovieAdapter.class.getSimpleName();
 
-    private int[] mDataSet;
+    private List<Movie> movieList;
+    private Context context;
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        Movie movie = getItem(position);
 
-        if (convertView == null)
-        {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_grid_item, parent, false);
+    public MovieAdapter(Context context, List<Movie> movieList)
+    {
+        this.context = context;
+        this.movieList = movieList;
+    }
+
+    public static class MovieViewHolder extends RecyclerView.ViewHolder
+    {
+        private ImageView moviePoster;
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+
+            this.moviePoster = (ImageView) itemView.findViewById(R.id.iv_movieposter);
         }
-
-        ImageView imageMovie = (ImageView) convertView.findViewById(R.id.iv_movieposter);
-        imageMovie.setImageResource(movie.image);
-
-
-        return super.getView(position, convertView, parent);
     }
 
     @Override
-    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public MovieViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType)
+    {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.movie_grid_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        MovieViewHolder viewHolder = new MovieViewHolder(view);
+
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(MovieViewHolder holder, int position) {
+    public void onBindViewHolder(MovieViewHolder holder, int position)
+    {
+        Log.d(TAG, "Movie" + position + "set.");
+        Movie movie = movieList.get(position);
+        String baseURL = "http://image.tmdb.org/t/p/";
+        String imageSize = "w185/";
 
+        Picasso.with(context)
+                .load(baseURL + imageSize + movie.getPoster_path())
+                .into(holder.moviePoster);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movieList.size();
     }
 
-    class MovieViewHolder extends RecyclerView.ViewHolder
-    {
-        private final ImageView listItemImageView;
 
-        public MovieViewHolder(View itemView) {
-            super(itemView);
-            itemView.setOnClickListener(new )
-            {
-
-            };
-            listItemImageView = (ImageView) itemView.findViewById(R.id.iv_movieposter);
-        }
-
-    }
 }
